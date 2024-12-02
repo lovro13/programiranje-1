7382175454f10bf0d272166bc1db792eeefb0f3e
lovro13 <lovro.zskrlj@gmail.com>
2024-12-02 10:19:59 +0100

    delo prejšnega tedna v šoli

--- 07-odvisni-tipi/vaje/logic.lean
+++ 07-odvisni-tipi/vaje/logic.lean
@@ -1,7 +1,18 @@
 -- Izomorfizmi
 
 theorem eq1 {A B : Prop} : (A ∧ B) ↔ (B ∧ A) :=
-  sorry
+  by
+    apply Iff.intro -- constructor sam pogleda če je dokaz ki ga rabimo
+    -- konstruktor, kot npr. ekvivalenca
+    intro ab
+    apply And.intro -- spet lahko uporabimo constructor
+    exact ab.right
+    exact ab.left
+    intro ab
+    apply And.intro
+    exact ab.right
+    exact ab.left
+
 
 theorem eq2 {A B : Prop} : (A ∨ B) ↔ (B ∨ A) :=
   sorry
@@ -16,7 +27,28 @@ theorem eq5 {A B C : Prop} : A ∧ (B ∨ C) ↔ (A ∧ B) ∨ (A ∧ C) :=
   sorry
 
 theorem eq6 {A B C : Prop} : (B ∨ C) → A ↔ (B → A) ∧ (C → A) :=
-  sorry
+  by
+    apply Iff.intro
+    intro h
+    apply And.intro
+    intro b
+    apply h
+    left
+    exact b
+
+    intro c
+    have xx : B ∨ C := Or.inr c
+    have yy := h (Or.inr c)
+    exact yy
+
+    intro h BvC
+    cases BvC
+    case inl b =>
+      have l := h.left
+      apply h.left
+      exact b
+    case inr c =>
+      exact h.right c
 
 theorem eq7 {A B C : Prop} : C → (A ∧ B) ↔ (C → A) ∧ (C → B) :=
   sorry
--- 07-odvisni-tipi/vaje/vector.lean
+++ 07-odvisni-tipi/vaje/vector.lean
@@ -36,48 +36,30 @@ inductive Finite : Naravno -> Type where
   | fsucc : {n : Naravno} -> Finite n -> Finite (Naravno.naslednik n)
 
 
-def downcast: a < b -> Finite a -> Finite b
-
 def lookup {A : Type} {n : Naravno} : Vektor A n -> Finite n -> A :=
-  sorry
+  fun xs i =>
+    match i, xs with
+    | Finite.fzero, Vektor.sestavljen y ys => y
+    | Finite.fsucc i', Vektor.sestavljen y ys =>
+    lookup ys i'
+
+
 
 
 -- Včasih enakost tipov ni takoj očitna in jo moramo izpeljati
 -- Dopolnite naslednjo definicijo, vse potrebne leme pa dokažite kar s taktiko `sorry`.
 
-def plus_zero (n : Naravno) : (plus n Naravno.nic) = n := by
-  sorry
-
-def plus_add_suc (m n : Naravno) : (plus m (Naravno.naslednik n)) = (Naravno.naslednik (plus m n)) := by
-  sorry
-
-def plus_comm (m n : Naravno) : (plus m n) = (plus n m) := by
-  sorry
-
--- xs ys
--- xs @ ys : Vector A (n + m)
--- xs @ ys : Vector A (m + n)
 def stakni_vektorja' : {A : Type} → {m n : Naravno} → Vektor A m → Vektor A n → Vektor A (plus n m) :=
-fun {A : Type} {m n : Naravno} (xs : Vektor A m) (ys : Vektor A n) =>
-  match xs with
-    | Vektor.prazen =>
-      by
-        rw [plus_zero]
-        exact ys
-    | Vektor.sestavljen x xs' =>
-      by
-        have aux := Vektor.sestavljen x (stakni_vektorja xs' ys)
-        rw [plus_add_suc, plus_comm]
-        exact aux
-
+  sorry --DN!!!
 
 -- Uporabite samo definicijo `stakni_vektorja'` in taktike `rw` in `exact`.
+
+def add_comm: {m n : Naravno} -> plus m n = plus n m
+:= sorry
+
 def stakni_vektorja'' : {A : Type} → {m n : Naravno} → Vektor A m → Vektor A n → Vektor A (plus m n) :=
-  fun {A : Type} {m n : Naravno} (xs : Vektor A m) (ys : Vektor A n) =>
-    by
-      have aux := stakni_vektorja' xs ys
-      rw [plus_comm]
-      exact aux
-
-
-#print stakni_vektorja''
+  by
+    intro A m n xs ys --taktika za dokazovanje implikacije
+    have xx := stakni_vektorja' xs ys
+    rw [add_comm]
+    exact xx
